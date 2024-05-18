9ae9b97582ed2ceb060831944709256cc71cfb20
Alvaro Sanchez <hello@alvarosanchez.dev>
2024-05-18 09:46:09 -0600

    Clicking on a board cell writes the internal rows property, according to the location of the cell

--- app/components/board.js
+++ app/components/board.js
@@ -1,7 +1,19 @@
 import Component from '@glimmer/component';
-
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 export default class BoardComponent extends Component {
-  get rows() {
-    return this.args.rows;
+  rowQuantity = 3;
+  columnQuantity = 3;
+  currentMarker = '💜';
+
+  @tracked rows = Array.from(
+    new Array(this.rowQuantity),
+    () => new Array(this.columnQuantity),
+  );
+
+  @action
+  writeBoard(rowIndex, columnIndex) {
+    this.rows[rowIndex][columnIndex] = this.currentMarker;
+    console.info(this.rows);
   }
 }
--- app/routes/game.js
+++ app/routes/game.js
@@ -1,22 +1,7 @@
 import Route from '@ember/routing/route';
-import { tracked } from '@glimmer/tracking';
-
 class Game {
   constructor() {
-    this.board = new Board();
     this.turn = 1;
-  }
-}
-
-class Board {
-  @tracked rowQuantity = 3;
-  @tracked columnQuantity = 3;
-
-  constructor() {
-    this.rows = Array.from(
-      new Array(this.rowQuantity),
-      () => new Array(this.columnQuantity),
-    );
   }
 }
 export default class GameRoute extends Route {
