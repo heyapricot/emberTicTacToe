25ac07b208b11d94a406f75efb0c6458dfeb553e
Alvaro Sanchez <hello@alvarosanchez.dev>
2024-05-18 18:55:29 -0600

    Lifted the tracked tows to the game controller to check for sequences. Game can check if there are vertical sequences

--- app/components/board.js
+++ app/components/board.js
@@ -1,19 +1,9 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { TrackedArray } from 'tracked-built-ins';
 export default class BoardComponent extends Component {
-  rowQuantity = 3;
-  columnQuantity = 3;
-
-  @tracked rows = Array.from(
-    new Array(this.rowQuantity),
-    () => new TrackedArray(Array(this.columnQuantity)),
-  );
-
   @action
   writeBoard(rowIndex, columnIndex) {
-    this.rows[rowIndex][columnIndex] = this.args.marker;
+    this.args.rows[rowIndex][columnIndex] = this.args.marker;
   }
 
   @action
--- app/controllers/game.js
+++ app/controllers/game.js
@@ -1,11 +1,18 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { TrackedArray } from 'tracked-built-ins';
 
 export default class GameController extends Controller {
   turn = 1;
   playerQuantity = 2;
+  rowQuantity = 3;
+  columnQuantity = 3;
   @tracked currentMarker = this.markers[0];
+  @tracked rows = Array.from(
+    new Array(this.rowQuantity),
+    () => new TrackedArray(Array(this.columnQuantity)),
+  );
 
   get markerAttrs() {
     return {
@@ -18,6 +25,10 @@ export default class GameController extends Controller {
     return Object.keys(this.markerAttrs);
   }
 
+  checkRows = () => {
+    return this.rows.some(this.hasRowSequence);
+  };
+
   increaseTurn() {
     this.turn++;
   }
@@ -26,9 +37,24 @@ export default class GameController extends Controller {
     this.currentMarker = this.markers[(this.turn + 1) % this.playerQuantity];
   }
 
+  hasRowSequence = (row) => {
+    return !row.includes(undefined) && row.every((elem) => elem === row[0]);
+  };
+
+  hasWinner = () => {
+    return this.checkRows();
+  };
+
+  announceWinner = () => {
+    if (!this.hasWinner()) return;
+
+    alert('Winner');
+  };
+
   @action
   onCellClick() {
     this.increaseTurn();
     this.toggleMarker();
+    this.announceWinner();
   }
 }
